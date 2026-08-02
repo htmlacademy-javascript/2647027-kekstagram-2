1181c485d5fa02b7e3f97bba3b6fab02e515b6b2
Keks <keks@htmlacademy.ru>
2026-08-02 16:06:09 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,3 @@
+import { getPhotos, COUNT_PHOTOS } from './data.js';
+
+getPhotos(COUNT_PHOTOS);
---
+++ 4/js/util.js
@@ -0,0 +1,10 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomElement = (items) => items[getRandomInteger(0, items.length - 1)];
+
+export {getRandomElement, getRandomInteger};
