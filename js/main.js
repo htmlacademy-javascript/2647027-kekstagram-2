5f5272a7631f5ea5edcf692c138050af5765b007
Юлия Море <yulia.more.web@yandex.ru>
2026-08-20 14:59:31 +0300

    9.13. Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { getPhotos, COUNT_PHOTOS } from './data.js';
 import './util.js';
 import { renderCards } from './render-cards.js';
+import './upload-form.js';
+import './validation.js';
 
 renderCards(getPhotos(COUNT_PHOTOS));
